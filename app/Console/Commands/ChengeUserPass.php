558865aba61f1786cb0f79b3f026000e07350253
asmi046 <asmi046@gmail.com>
2024-09-24 02:32:13 +0300

    new command

--- app/Console/Commands/ChengeUserPass.php
+++ app/Console/Commands/ChengeUserPass.php
@@ -8,8 +8,9 @@ use App\Mail\CronTestMail;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Contracts\Console\PromptsForMissingInput;
 
-class ChengeUserPass extends Command
+class ChengeUserPass extends Command  implements PromptsForMissingInput
 {
     /**
      * The name and signature of the console command.
@@ -24,6 +25,20 @@ class ChengeUserPass extends Command
      * @var string
      */
     protected $description = 'Новый пароль для пользователя';
+
+
+    /**
+     * Prompt for missing input arguments using the returned questions.
+     *
+     * @return array<string, string>
+     */
+    protected function promptForMissingArgumentsUsing(): array
+    {
+        return [
+            'email' => 'Введите e-mail:',
+            'pass' => 'Введите пароль:',
+        ];
+    }
 
     /**
      * Execute the console command.
