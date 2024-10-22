7ae7ed6ce890425911fb450f9e83914a220302c8
asmi046 <asmi046@gmail.com>
2024-10-22 03:18:15 +0300

    Дайджест на главыной

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pay;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function __invoke() {
-        return Inertia::render('Index');
+        $digest = Pay::selectRaw('client, COUNT(*) as count, SUM(price) as summ')->groupBy('client')->get();
+        $price_total = Pay::all()->sum('price');
+        return Inertia::render('Index', [
+            'worcprice' => $digest,
+            'total' => $price_total,
+        ]);
     }
 }
--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -2,15 +2,32 @@
     <Head>
         <title>Главная</title>
     </Head>
-    <h1>111111</h1>
-    <Button label="Submit" />
+    <div class="card">
+        <DataTable :value="props.worcprice" tableStyle="min-width: 50rem">
+            <Column field="client" header="Клиент"></Column>
+            <Column field="count" header="Выполнено работ"></Column>
+            <Column field="summ" header="Сумма">
+                <template #body="{ data, field }">
+                    {{ formatCurrency(data[field]) }}
+                </template>
+            </Column>
+        </DataTable>
+    </div>
+    <p>
+        Итого: <strong>{{ formatCurrency(props.total) }}</strong>
+    </p>
 </template>
 
 <script setup>
-import Button from 'primevue/button';
-
-
 import { Head } from '@inertiajs/vue3'
 
+const props = defineProps({
+    worcprice: Array,
+    total: Number
+})
+
+const formatCurrency = (value) => {
+    return new Intl.NumberFormat('ru-RU', { style: 'currency', currency: 'RUB' }).format(value);
+}
 
 </script>
